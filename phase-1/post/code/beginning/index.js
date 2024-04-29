d53c68fee38b5ad1e41921a1614c7575ad5c018b
Alina Rudanets <82982601+AllinaArr@users.noreply.github.com>
2024-04-29 17:41:56 -0400

    modified

--- phase-1/post/code/beginning/index.js
+++ phase-1/post/code/beginning/index.js
@@ -17,19 +17,29 @@ function handleDogCreation(dog) {
   li.textContent = `${dog.name} (${dog.age})`;
   ul.append(li);
 }
+
+function handleCatCreation(cat) {
+  const ul = document.querySelector("#cats");
+  const li = document.createElement("li");
+  li.textContent = `${cat.name} (${cat.age})`;
+  ul.append(li);
+}
 // Let's try making a GET request to display existing data on the page.
-fetch("http://localhost:3000/dogs")
-  .then((response) => {
-    return response.json();
-  })
-  .then((dogs) => {
-    dogs.forEach(handleDogCreation);
-    // dogs.forEach((dog) => {
-    //   const li = document.createElement("li");
-    //   li.textContent = `${dog.name} (${dog.age})`;
-    //   ul.append(li);
-    // });
-  });
+// let value = "cats";
+
+function getResponse(value) {
+  fetch(`http://localhost:3000/${value}`)
+    .then((response) => response.json())
+    .then((data) => {
+      if (value === "dogs") {
+        data.forEach(handleDogCreation);
+      } else {
+        data.forEach(handleCatCreation);
+      }
+    });
+}
+getResponse("cats");
+getResponse("dogs");
 
 // Now, let's trigger a POST request when the user submits the form,
 // so that they can add data to the database! Remember to think about
@@ -55,6 +65,34 @@ document.querySelector("form").addEventListener("submit", (event) => {
 
 // ~ Challenges
 // 1. There are a handful of awfully similar lines in our requests. Try abstracting them
-//    into a function!
+//    into a function! -> done
 // 2. Try writing your own POST request.
+
 // 3. Try writing PATCH and DELETE requests!
+
+//---------
+// function fetchPetData(catsOrDogs) {
+//   fetch(`http://localhost:3000/${catsOrDogs}`)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       data.forEach((pet) => {
+//         createPet;
+//         console.log(pet);
+//       });
+//     });
+
+//   function createPet() {
+//     const uldog = document.querySelector("#dogs");
+//     const lidog = document.createElement("li");
+//     lidog.textContent = `${dog.name} (${dog.age}))`;
+//     uldog.append(lidog);
+
+//     const ulcat = document.querySelector("#cats");
+//     const licat = document.createElement("li");
+//     licat.textContent = `${cat.name} (${cat.age}))`;
+//     ulcat.append(licat);
+//   }
+// }
+
+// fetchPetData("dogs");
+// fetchPetData("cats");
