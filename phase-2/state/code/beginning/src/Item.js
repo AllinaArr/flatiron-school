1d6cde1e144b50858782559e7b433957b2bff460
Alina Rudanets <82982601+AllinaArr@users.noreply.github.com>
2024-05-14 18:07:03 -0400

    state practice

--- phase-2/state/code/beginning/src/Item.js
+++ phase-2/state/code/beginning/src/Item.js
@@ -1,20 +1,29 @@
+import { useState } from "react";
 // To destructure an object within an object, separate the "nests" you want with a colon.
 // Writing `export default` before our component saves us from writing `export default [function]` at the end of the file.
-export default function Item({ item: { name, price } }) {
+export default function Item({ itemProp }) {
+  const [isInCart, setIsInCart] = useState(false);
+
+  function toggleCart() {
+    setIsInCart(!isInCart);
+  }
+
   return (
     <li>
       {/* Since we destructured props.item, we save ourselves from writing `item.name` and `item.price`. */}
-      <h2>{name} for only ${price}!</h2>
+      <h2>
+        {itemProp.name} for only ${itemProp.price}!
+      </h2>
       <p>
-        <button>
-          <strong>Add to Cart</strong>
+        <button onClick={toggleCart}>
+          <strong>{isInCart ? "Remove from Card" : "Add to Cart"}</strong>
         </button>
         {/* Some manual spacing, nothing to see here... */}
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <span>Not in Cart</span>
+        <span>{isInCart ? "In Card" : "Not in Cart"}</span>
       </p>
       {/* Some manual spacing between <Item> components, nothing to see here... */}
-      <br/>
+      <br />
     </li>
   );
 }
