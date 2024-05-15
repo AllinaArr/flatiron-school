e3b55fa89860c3cc5dcc4b867ea2a3981cd5e8b6
Alina Rudanets <82982601+AllinaArr@users.noreply.github.com>
2024-05-15 13:57:11 -0400

    completed

--- phase-2/control/code/beginning/src/Card.js
+++ phase-2/control/code/beginning/src/Card.js
@@ -11,7 +11,9 @@ export default function Card({ isCard, randomize }) {
         borderRadius: "10px",
       }}
     >
+      {/* //5. [Inside card] We need to replace the static card text with the state value. */}
       {isCard}
+      {/* //4. [Inside card] Add a click event listener to the button that triggers that functions. */}
       <button onClick={randomize}>Nah.</button>
     </h2>
   );
