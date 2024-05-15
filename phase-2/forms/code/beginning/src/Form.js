bf5d083b8d9c30bb8f9c23a3be8389df343329ef
Alina Rudanets <82982601+AllinaArr@users.noreply.github.com>
2024-05-15 17:59:44 -0400

    Almost done

--- phase-2/forms/code/beginning/src/Form.js
+++ phase-2/forms/code/beginning/src/Form.js
@@ -1,19 +1,48 @@
+import { useState } from "react";
+
 export default function Form() {
-    return (
-        <form>
-            <label><strong>Name </strong>
-                <input />
-            </label>
-            <br/><br/>
-            <label><strong>Movie </strong>
-                <input />
-            </label>
-            <br/><br/>
-            <em>Your lucky number is</em> <strong>2</strong>!
-            <br/><br/>
-            <input type="submit" value="Sign Up"/>
-            <h2>Members</h2>
-            <sub>Fill out the form to add your first member!</sub>
-        </form>
-    )
+  //   const [name, setName] = useState("");
+  //   const [movie, setMovie] = useState("");
+
+  const [memberData, setMemberData] = useState({ name: "", movie: "" });
+
+  const handleChange = (event) => {
+    setMemberData({
+      ...memberData,
+      [event.target.name]: event.target.value,
+    });
+
+    if (memberData.movie.length >= 10) {
+      alert("Only 10 characters are allowed ");
+    }
+
+    if (/\d/.test(event.target.value)) {
+      alert("No numbers are allowed in the name!");
+      return;
+    }
+  };
+
+  return (
+    <form>
+      <label>
+        <strong>Name </strong>
+        <input name='name' value={memberData.name} onChange={handleChange} />
+      </label>
+      <br />
+      <br />
+      <label>
+        <strong>Movie </strong>
+        <input name='movie' value={memberData.movie} onChange={handleChange} />
+      </label>
+      <br />
+      <br />
+      <em>Your lucky number is</em>{" "}
+      <strong>{memberData.name.length * memberData.movie.length}</strong>!
+      <br />
+      <br />
+      <input type='submit' value='Sign Up' />
+      <h2>Members</h2>
+      <sub>Fill out the form to add your first member!</sub>
+    </form>
+  );
 }
